cfedf20f27d4059ba328203e0db4b3bfc20fec11
MDEAGEWT <kmk6061602@naver.com>
2025-07-30 22:31:07 +0900

    make mesh folder if doesn't exist

--- Tools/download_ctrack.sh
+++ Tools/download_ctrack.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-python3 -m gdown --continue 13TmnltIcraQ770KEB1bI-RRVQ1CgvL_1 -O $SCRIPT_DIR/simulation/gazebo-classic/sitl_gazebo-classic/models/c-track/meshes/c-track.stl
+DEST_PATH="$SCRIPT_DIR/simulation/gazebo-classic/sitl_gazebo-classic/models/c-track/meshes"
+if [ ! -d "$DEST_PATH" ]; then
+  mkdir "$DEST_PATH"
+fi
+
+python3 -m gdown --continue 13TmnltIcraQ770KEB1bI-RRVQ1CgvL_1 -O "$DEST_PATH/c-track.stl"
